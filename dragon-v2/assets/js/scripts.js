fabe9e13a0e4e11c87e403ededf75d116db6e474
Chris <vincentvalentine1320@yahoo.com>
2022-12-07 11:17:15 +0000

    buttons

--- dragon-v2/assets/js/scripts.js
+++ dragon-v2/assets/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+//autoplay slideshow
     $('.autoplay').slick({
       slidesToShow: 3,
       slidesToScroll: 1,
@@ -7,5 +7,26 @@ $(document).ready(function() {
       autoplaySpeed: 2500
     });
 
+//back to top button
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+})
+
+const goToTop = () => {
+  document.body.scrollIntoView({behavior:"smooth"});
+};
+
+backToTopButton.addEventListener("click", goToTop)
 
 });
